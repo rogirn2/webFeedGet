b4a760f02f7e8334f08324093c85da33a2001ed0
Igor Alves <igor.brito.alves@outlook.com.br>
2022-06-03 16:52:47 -0300

    Criando componente  FeedbackContent

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+import { CloseButton } from "../CloseButton";
+
+import bugImage from "../../assets/bug.svg";
+import ideaImage from "../../assets/idea.svg";
+import thoughtImage from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+    BUG : {
+        title: 'Problema',
+        image: {
+            source: bugImage,
+            alt: "Imagem de um inseto"
+        }
+    },
+    IDEA : {
+        title: 'Ideia',
+        image: {
+            source: ideaImage,
+            alt: "Imagem de uma lâmpada"
+        }
+    },
+    OTHER : {
+        title: 'Outro',
+        image: {
+            source: thoughtImage,
+            alt: "Imagem de uma nuvem de pensamento"
+        }
+    }
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    function handleRestartFeedback(){
+        setFeedbackType(null);
+    }
+
+    return(
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2.5rem)] md:w-auto">
+
+            {!feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+                <FeedbackContentStep 
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback} 
+                />
+            )}
+
+            <footer className="text-xs text-neutral-400">
+                Feito com ♥ por <a className="underline underline-offset-2" href="https://github.com/rogirn2">rogirn2</a>
+            </footer>
+        </div>
+    );
+}
