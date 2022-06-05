418621753521467df30564aa320708e0854d2276
Igor Alves <igor.brito.alves@outlook.com.br>
2022-06-05 16:28:33 -0300

    Finalizando feedback content

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import ideaImage from "../../assets/idea.svg";
 import thoughtImage from "../../assets/thought.svg";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG : {
@@ -36,6 +37,7 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
     function handleRestartFeedback(){
         setFeedbackType(null);
@@ -44,13 +46,20 @@ export function WidgetForm() {
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2.5rem)] md:w-auto">
 
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            { feedbackSent ? (
+                <FeedbackSuccessStep />
             ) : (
-                <FeedbackContentStep 
-                    feedbackType={feedbackType}
-                    onFeedbackRestartRequested={handleRestartFeedback} 
-                />
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep 
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)} 
+                        />
+                    )}
+                </>
             )}
 
             <footer className="text-xs text-neutral-400">
